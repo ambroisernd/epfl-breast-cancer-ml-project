7b43b7f38ed8c3f1352cf5ba96cfcfb193d6cb59
Gautier <piallagautier@gmail.com>
2019-12-18 18:28:25 +0100

    generate_prediction.py can make the predictions for every model, default path in data/predictions

--- src/generate_prediction.py
+++ src/generate_prediction.py
@@ -9,18 +9,31 @@ from sklearn.ensemble  import RandomForestClassifier
 from sklearn.linear_model  import LogisticRegression
 from sklearn.decomposition import PCA
 
+def features_to_keep():
+    df_important_genes = pd.read_csv("../data/important_genes.csv", sep=",")
+    important_genes = list(df_important_genes["value"].unique())
+    
+    df_test = pd.read_csv("../data/normalized_test.csv")
+    genes_test = list(df_test.columns)
+    
+    return [gene for gene in important_genes if gene in genes_test]
+    
 
 def load_dataset(path_dataset, training=True):
+    
     # Open CSV
     df = pd.read_csv(path_dataset, sep=",")
-    X = df[df.columns.difference(["full_label", "label"])].values
+    # Keep only common features
+    features = features_to_keep()
+    X = df[features].values
+    
     if training:
         # Get y for training
         y           = df["label"].values
-        full_labels = df["full_labels"].values
-        return X, y, full_labels
+        return X, y
     else:
-        return X
+        full_labels = df["full_label"].values
+        return X, full_labels
 
 def get_default_model(type_model):
     # Return model with default values
@@ -46,8 +59,8 @@ def preprocess_data(X_train, X_pred, model_type):
 
 def main(model_type, saving_path, dataset_path, unlabeled_dataset_path):
     # Load datasets
-    X_train, y_train, full_labels = load_dataset(dataset_path, training=True)
-    X_pred = load_dataset(unlabeled_dataset_path, training=False)
+    X_train, y_train    = load_dataset(dataset_path, training=True)
+    X_pred, full_labels = load_dataset(unlabeled_dataset_path, training=False)
     
     # Get model
     model = get_default_model(model_type)
@@ -69,15 +82,18 @@ def main(model_type, saving_path, dataset_path, unlabeled_dataset_path):
     df_predictions["proba_0"]    = y_proba[:,0]
     df_predictions["proba_1"]    = y_proba[:,1]
     df_predictions["proba_2"]    = y_proba[:,2]
+    
+    name_output = os.path.join(saving_path, model_type+"_preds.csv")
     df_predictions.to_csv(saving_path, index=False)
 
 
 if __name__ == "__main__":
     # Managing the arguments
+    
     ap = argparse.ArgumentParser()
     ap.add_argument("-model", "--model", required=True,
-                    help="The model do to the predictions with")    
-    ap.add_argument("-saving_path", "--saving_path", required=True,
+                    help="The model do to the predictions with KNN, DecisionTrees, LogReg, RndForest")    
+    ap.add_argument("-saving_path", "--saving_path", required=False,
                     help="To path were to save the predictions")
     ap.add_argument("-dataset_path", "--dataset_path", required=False,
                     help="The path of the dataset to use in order to train the model")
@@ -88,14 +104,22 @@ if __name__ == "__main__":
 
     # Get arguments
     model_type  = args["model"]
+    model_types = ["KNN", "DecisionTrees", "LogReg", "RndForest"]
+    if model_type not in model_types:
+        raise Exception("Model type is not valid, valid types are: " + str(model_types))
+        exit()
+    
     saving_path = args["saving_path"]
 
     # Defaults values
+    if args["saving_path"] == None:
+        dataset_path = "../data/predictions/"
+    
     if args["dataset_path"] == None:
-        dataset_path = ""   # TODO: Set default path
+        dataset_path = "../data/normalized_train.csv"
 
     if args["unlabeled_dataset_path"] == None:
-        unlabeled_dataset_path = "" # TODO: Set default path
+        unlabeled_dataset_path = "../data/normalized_test.csv"
 
     # Call main
     main(model_type, saving_path, dataset_path, unlabeled_dataset_path)
